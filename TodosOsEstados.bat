8f6d346dde2becafa94f786c72bb525390d6546d
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-21 16:58:40 -0300

    estados brasil

--- TodosOsEstados.bat
+++ TodosOsEstados.bat
@@ -7,3 +7,50 @@ md "Norte"
 md "Sul" 
 md "Sudeste"
 
+cd Centro-Oeste
+md Goias
+md "Mato Grosso"
+md "Mato Grosso do Sul"
+md "Distrito Federal"
+
+cd..
+
+cd Nordeste
+md Alagoas
+md Bahia
+md Ceara
+md Maranhao
+md Paraiba
+md Pernambuco
+md Piaui
+md "Rio Grande do Norte"
+md Sergipe
+
+cd ..
+
+cd Norte
+md Acre
+md Amapa
+md Amazonas
+md Para
+md Rondonia
+md Roraima
+md Tocantins
+
+cd ..
+
+cd Sul
+md Parana
+md "Santa Catarina"
+md "Rio Grande do Sul"
+
+cd ..
+
+cd Sudeste
+md "Sao Paulo"
+md "Rio de Janeiro"
+md "Espirito Santo"
+md "Minas Gerais"
+
+
+cd ..
--- TodosOsPaises.bat
+++ TodosOsPaises.bat
@@ -1,2 +1,45 @@
 md "Todos os Paises"
 cd "Todos os Paises"
+
+md Argentina
+md Bolivia
+md Brasil 
+md Chile
+md Colombia
+md Equador
+md Guiana
+md "Guiana Francesa"
+md Paraguai
+md Peru
+md Suriname
+md Uruguai
+md Venezuela
+
+cd Brasil
+
+md Acre
+md Alagoas
+md Amapa
+md Amazonas
+md Bahia
+md Ceara
+md "Espirito Santo"
+md "Goias"
+md "Maranhao"
+md "Mato Grosso"
+md "Mato Grosso do Sul"
+md "Minas Gerais"
+md "Para"
+md "Paraiba"
+md "Parana"
+md "Pernambuco"
+md "Piaui"
+md "Rio de Janeiro"
+md "Rio Grande do Norte"
+md "Rio Grande do Sul"
+md "Rondonia"
+md "Roraima"
+md "Santa Catarina"
+md "Sao Paulo"
+md Sergipe
+md Tocantins
